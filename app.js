f681266303dbae6f110c434df2a828b9a187012b
m272xu <m272xu@uwaterloo.ca>
2021-01-06 00:11:44 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const articleSchema = {
+  title: String,
+  content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
+
+////////////// Requests Targetting ALL articles //////////
+app.route("/articles")
+.get(function(req, res) {
+  Article.find(function(err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res) {
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err) {
+    if (!err) {
+      res.send("New article saved");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res) {
+  Article.deleteMany(function(err) {
+    if (!err) {
+      res.send("Successfully deleted everything");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+////////////// Requests Targetting ALL articles //////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res) {
+  Article.findOne(
+    {title: req.params.articleTitle},
+    function(err, foundArticle) {
+    if (!err) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No article found");
+      }
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.put(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    function(err){
+      if (!err) {
+        res.send("Article updated");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.patch(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err) {
+      if (!err) {
+        res.send("Successfully patched article");
+      } else {
+        res.send(err);
+      }
+    }
+  )
+})
+
+.delete(function(req, res) {
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err) {
+      if (!err) {
+        res.send("Successfully deleted article");
+      } else {
+        res.send(err);
+      }
+    }
+  )
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
